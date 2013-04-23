84298d2b467195196e4de82bbee5f08fd9d55c7a
kodam <hotsoup.h@gmail.com>
2013-04-24 01:47:08 +0900

    update for localization

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,5 +4,9 @@ require_once('config/config.php');
 execute();
 
 function defaultAction(){
-  include_layout('inc_index.php');
+  if(isset($_GET['lang']) && file_exists('inc_index.'.$_GET['lang'].'.php')){
+    include_layout('inc_index.'.$_GET['lang'].'.php');
+  }else{
+    include_layout('inc_index.php');
+  }
 }
